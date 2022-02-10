7a384eab46a610c688c4dda69037b497615cfe1d
cassandrayuen <yuen.cassandra.i@gmail.com>
2022-02-10 11:11:27 +1100

    Add inputs for testing from HIA workshop 2021 Perth 2014-2016. packages.R now installs only, specifies to load in _targets.R

--- _targets.R
+++ _targets.R
@@ -1,10 +1,43 @@
 library(targets)
 library(tarchetypes)
 
+# source globals
+datadir <- "~/../cloudstor/Shared/Environment_General/"
 sapply(list.files(pattern="[.]R$", path="R/", full.names=TRUE), source)
 
-list(
+# Set target-specific options such as packages.
+tar_option_set(packages = c("data.table",
+                            "sf",
+                            "raster"))
 
+list(
+#### Inputs ####
+## geographical geom
+tar_target(exp_geog_raw_f, file.path(datadir, "ABS_data/ABS_meshblocks/abs_meshblocks_2016_data_provided/MB_2016_VIC.shp"), format = "file"),
+
+## pollutant concentration
+tar_target(exposure1_raw_f, file.path(datadir, "Air_pollution_model_GlobalGWR_PM25/GlobalGWR_PM25_V4GL02/data_derived/GlobalGWR_PM25_GL_201501_201512-RH35-NoNegs_AUS_20180618.tif"), format = "file"),
+## counterfactual
+# ??? numeric/raster/csv/shape
+## meshblock pops
+tar_target(exp_pop_raw_f, file.path(datadir, "ABS_data/ABS_meshblocks/abs_meshblocks_2016_pops_data_provided/2016 census mesh block counts.csv"), format = "file"),
+# mb_pops_varlist <- c("MB_CODE16", "MB_CATEGORY_NAME_2016", "Person")
+
+## impact
+tar_target(impact_pop_f, file.path(datadir, "Australian_Mortality_ABS/ABS_MORT_2006_2016/data_provided/DEATHS_AGESPECIFIC_OCCURENCEYEAR_04042018231304281.csv"), format = "file"),
+# indat_death_varlist <- c("Region", "Sex", "Age", "Measure", "Time", "Value")
+## baseline pop stats
+tar_target(study_pop_f, file.path(datadir, "ABS_data/ABS_Census_2016/abs_gcp_2016_data_derived/abs_sa2_2016_agecatsV2_total_persons_20180405.csv"), format = "file"),
+
+#### Load data ####
+tar_target(exp_geog_raw, st_read(exp_geog_raw_f)),
+tar_target(exposure1_raw, raster(exposure1_raw_f)),
+tar_target(exp_pop_raw, fread(exp_pop_raw_f)),
+tar_target(impact_pop, fread(impact_pop_f)),
+tar_target(study_pop, fread(study_pop_f)),
+
+
+#### Analysis ####
 tar_target(health_impact_function,
            do_health_impact_function(
              case_definition = 'crd',
@@ -22,7 +55,8 @@ tar_target(dat_linked_pop_health_enviro,
 
 tar_target(dat_exposure1_prep,
              load_exposure1(
-               exposure1_raw
+               exposure1_raw,
+               exp_geog_raw
              )
              ),
 
--- main.R
+++ main.R
@@ -3,7 +3,23 @@ library(tarchetypes)
 
 source("R/packages.R")
 
+#### Debug ####
+library(data.table)
+library(sf)
+library(raster)
+
+tar_manifest()
+# tar_option_set(debug = "health_impact_function")
+# tar_make(names = health_impact_function, callr_function = NULL)
+
+
+# tar_manifest(fields = "command")
+# tar_glimpse(targets_only = FALSE)
+
+#### Run ####
+# tar_make() # run pipeline
+# tar_read(prep_exposure_pm25) # see results of specified target
+
 tar_visnetwork()
-#tar_make()
 
 #browseURL("index.html")
