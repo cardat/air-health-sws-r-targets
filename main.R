f3324f676b9251817bb35124d481f01472581286
Ivan Hanigan <ivan.hanigan@gmail.com>
2022-03-09 13:51:49 +0800

    add rmarkdown for gitbook

--- main.R
+++ main.R
@@ -1,9 +1,14 @@
 library(targets)
 library(tarchetypes)
+library(bookdown)
 
 source("R/packages.R")
 
 tar_visnetwork()
 #tar_make()
 
-#browseURL("index.html")
+showme <- FALSE
+if(showme){
+  render_book( "index.Rmd", gitbook(split_by = "section", self_contained = FALSE, config = list(sharing = NULL, toc = list(collapse = "section"))) )
+  browseURL("index.html")
+}
