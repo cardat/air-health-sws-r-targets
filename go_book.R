bdbabfe81f5db7ce87a43564c5f555214deeb0d5
Ivan Hanigan <ivan.hanigan@gmail.com>
2022-03-09 14:29:02 +0800

    add working Rmarkdown to bookdown

---
+++ go_book.R
@@ -0,0 +1,9 @@
+library(bookdown)
+
+render_book( "index.Rmd", gitbook(split_by = "section", self_contained = FALSE, config = list(sharing = NULL, toc = list(collapse = "section"))) )
+
+flist <- dir("_book", full.names = T)
+for(fi in flist){
+  #fi <- flist[1]
+  file.rename(fi, gsub("_book/", "", fi))
+}
