7f57e792727c17fd61b815a63e6d4235d4ad79df
Ivan Hanigan <ivan.hanigan@gmail.com>
2022-03-10 11:13:43 +0800

    commit Rmd templat for book but not the html

--- main.R
+++ main.R
@@ -1,14 +1,9 @@
 library(targets)
 library(tarchetypes)
-library(bookdown)
 
 source("R/packages.R")
 
 tar_visnetwork()
 #tar_make()
 
-showme <- FALSE
-if(showme){
-  render_book( "index.Rmd", gitbook(split_by = "section", self_contained = FALSE, config = list(sharing = NULL, toc = list(collapse = "section"))) )
-  browseURL("index.html")
-}
+#browseURL("index.html")
