f005b7b1f806197afd416741e601977c37cf591f
cassandrayuen <yuen.cassandra.i@gmail.com>
2022-03-15 13:28:31 +1100

    Improve mapping

--- R/func_viz/viz_map_an.R
+++ R/func_viz/viz_map_an.R
@@ -5,5 +5,9 @@
 
 viz_map_an <- function(sf, field){
   ggplot(sf) + 
-    geom_sf(mapping = aes(fill = attributable), lwd = 0)
+    geom_sf(mapping = aes(fill = attributable, col = attributable), lwd = 0) +
+    facet_grid(~year) +
+    scale_fill_viridis_c() + scale_colour_viridis_c() #+ 
+    
+    # coord_sf(xlim = c(115, 117), ylim = c(-33, -31), expand = TRUE) # perth
 }
