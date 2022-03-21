4fb8ac9232c8ae79fb1b623fd3333e96c94d4fbd
cassandrayuen <yuen.cassandra.i@gmail.com>
2022-03-21 16:17:08 +1100

    Tidy main.R to run from scratch

--- main.R
+++ main.R
@@ -1,16 +1,50 @@
-library(targets)
-library(tarchetypes)
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+##
+## Construct and run a Health Impact Assessment (HIA) pipeline
+##
+##
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+#### Restore environment with renv ####
+## Opening this project should have autoloaded renv - if not, use install.package("renv") to install then renv::activate() to load the renv lockfile
+
+# restore the environment
+renv::restore()
+
+#### Load libraries ####
+library(targets)
+
+#### Write the pipeline ####
+# load function for creating _targets.R for HIA pipeline
 source("R/pipelines/write_pipeline_perth.R")
 
-write_pipeline_perth(preset = "Perth_2014_2016")
+# write _targets.R file
+write_pipeline_perth(states = c("NSW", "ACT"), # study area
+                     years = 2013:2014 # temporal coverage
+                     )
+
+#### Run pipeline ####
+
+# visualise targets
+tar_glimpse()
+# run pipeline
+tar_make()
+
+# visualise target status
+tar_visnetwork(targets_only = T)
+
+#### View target output ####
+
+tar_read(prep_exposure_pm25) # see results of specified target
+
+#browseURL("index.html")
+
 
 # #### Debug ####
 # library(data.table)
 # library(sf)
 # library(raster)
 # library(exactextractr)
-# 
 
 # tar_destroy()
 # tar_manifest()
@@ -22,11 +56,3 @@ write_pipeline_perth(preset = "Perth_2014_2016")
 
 # tar_manifest(fields = "command")
 # tar_glimpse(targets_only = FALSE)
-
-#### Run ####
-tar_make() # run pipeline
-# tar_read(prep_exposure_pm25) # see results of specified target
-
-tar_visnetwork(targets_only = T)
-
-#browseURL("index.html")
