f26c45fd68af649c7138136ed604684280ffbc78
ivanhanigan <ivan.hanigan@gmail.com>
2024-03-07 07:34:11 +0000

    remove unsupported tar_read from inside target

--- R/import_data/import_globalgwr_pm25_2010_2015.R
+++ R/import_data/import_globalgwr_pm25_2010_2015.R
@@ -52,7 +52,7 @@ import_globalgwr_pm25_2010_2015 <- function(
     substitute({
       b <- brick(stack(infile_globalgwr_pm25_2010_2015))
       # retrieve year for layer label
-      names(b) <- basename(tar_read(infile_globalgwr_pm25_2010_2015))
+      names(b) <- basename(infile_globalgwr_pm25_2010_2015)
       names(b) <- gsub("GlobalGWR_PM25_GL_([0-9]{4})01.*", "\\1", names(b))
       return(b)
     })
