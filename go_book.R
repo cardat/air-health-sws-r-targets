20adafd953e1fac8e53581f43c7d3b1195d404ab
cassandrayuen <yuen.cassandra.i@gmail.com>
2025-07-14 15:45:11 +1000

    Integrate Readme from LH to existing gh-pages

--- go_book.R
+++ go_book.R
@@ -1,10 +1,13 @@
 library(bookdown)
 
-render_book( "index.Rmd", gitbook(split_by = "section", self_contained = FALSE, config = list(sharing = NULL, toc = list(collapse = "section"))) )
+render_book( "index.Rmd", gitbook(split_by = "chapter", 
+                                  self_contained = FALSE, 
+                                  config = list(sharing = NULL, toc = list(collapse = "section"))) )
 
 flist <- dir("_book", full.names = T)
 for(fi in flist){
   #fi <- flist[1]
   file.rename(fi, gsub("_book/", "", fi))
 }
-## browseURL("index.html")
+browseURL("index.html")
+  
